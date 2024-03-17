61d6c44b0338fe5414253b0361f521981b706484
nguyengiabinh <115540912+nguyengiabinh@users.noreply.github.com>
2024-03-17 23:10:07 +0700

    update
    added circle, isarael gaza circle is color green while the other blue

--- example code for palestine geomap/8.1.js
+++ example code for palestine geomap/8.1.js
@@ -1,17 +1,104 @@
+// var w = 1000;
+// var h = 1000;
+// var padding = 50;
+
+// var color = d3.scaleOrdinal()
+//     .range(["#FFFF00", "#FFA500", "#F08080", "#FF0000"]);
+
+// // Define projection settings
+// var projection = d3.geoMercator()
+//                    .center([35.2, 31.9]) // Adjusted center for Palestine
+//                    .scale(25000) // Adjusted scale for better fit
+//                    .translate([w / 2, h / 2]);
+
+// var path = d3.geoPath()
+//              .projection(projection);
+
+// // Start the SVG block
+// var svg = d3.select("body")
+//     .append("svg")
+//     .attr("width", w)
+//     .attr("height", h)
+//     .attr("class", "img");
+
+// d3.json("palestine-with-regions_1504.geojson").then(function(json) {
+
+//     // Draw GeoJSON features
+//     svg.selectAll("path")
+//         .data(json.features)
+//         .enter()
+//         .append("path")
+//         .attr("stroke", "dimgray")
+//         .attr("fill", function(d, i) {
+//             return color(i);
+//         })
+//         .attr("d", path);
+// });
+
+
+//     d3.json("palestine-with-regions_1504.geojson").then(function(json){
+
+//         svg.selectAll("path")
+//             .data(json.features)
+//             .enter()
+//             .append("path")
+//             .attr("stroke", "dimgray")
+//             .attr("fill", function(d, i) {return color(i)})
+//             .attr("d", path);
+
+//         d3.csv("event_data_pse.csv", function(d){
+//             return {
+//                 lat: +d.latitude,
+//                 long: +d.longitude
+//             }
+//         }).then(function(data){
+            
+//             svg.selectAll("circle")
+//             .data(data)
+//             .enter()
+//             .append("circle")
+//             .attr("cx", function(d, i){
+//                 return projection([d.long, d.lat])[0];
+//             })
+//             .attr("cy", function(d, i){
+//                 return projection([d.long, d.lat])[1];
+//             })
+//             .attr("r", 5);
+        
+//             svg.selectAll("text")
+//             .data(data)
+//             .enter()
+//             .append("text")
+//             .attr("x", function(d, i){
+//                 return projection([d.long, d.lat])[0];
+//             })
+//             .attr("y", function(d, i){
+//                 return projection([d.long, d.lat])[1];
+//             })
+//             .style("fill", d3.color("#007FFF"))
+//             .style("z-index", 1);
+//         });
+//     });
+
+
+
+
+
 var w = 1000;
 var h = 1000;
 var padding = 50;
 
-var color = d3.scaleOrdinal(d3.schemeCategory10);
+var color = d3.scaleOrdinal()
+    .range(["#FFFF00", "#FFA500", "#F08080", "#FF0000"]);
 
 // Define projection settings
 var projection = d3.geoMercator()
-                   .center([35.2, 31.9]) // Adjusted center for Palestine
-                   .scale(25000) // Adjusted scale for better fit
-                   .translate([w / 2, h / 2]);
+    .center([35.2, 31.9]) // Adjusted center for Palestine
+    .scale(25000) // Adjusted scale for better fit
+    .translate([w / 2, h / 2]);
 
 var path = d3.geoPath()
-             .projection(projection);
+    .projection(projection);
 
 // Start the SVG block
 var svg = d3.select("body")
@@ -20,7 +107,7 @@ var svg = d3.select("body")
     .attr("height", h)
     .attr("class", "img");
 
-d3.json("palestine-with-regions_1504.geojson").then(function(json) {
+d3.json("palestine-with-regions_1504.geojson").then(function (json) {
 
     // Draw GeoJSON features
     svg.selectAll("path")
@@ -28,8 +115,37 @@ d3.json("palestine-with-regions_1504.geojson").then(function(json) {
         .enter()
         .append("path")
         .attr("stroke", "dimgray")
-        .attr("fill", function(d, i) {
+        .attr("fill", function (d, i) {
             return color(i);
         })
         .attr("d", path);
 });
+
+
+d3.csv("event_data_pse.csv", function(d) {
+    return {
+        lat: +d.latitude,
+        long: +d.longitude,
+        event_name: d.event_name // No need to convert it to a number
+    }
+}).then(function(data) {
+
+    svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d, i) {
+            return projection([d.long, d.lat])[0];
+        })
+        .attr("cy", function(d, i) {
+            return projection([d.long, d.lat])[1];
+        })
+        .attr("r", 5)
+        .style("fill", function(d) {
+            // Check if the event_name string contains the substring
+            return d.event_name.includes("Palestine: Unknown (IAC) (Hostilities Gaza/Israel) - Gaza Strip") ? "green" : "blue";
+        });
+})
+
+
+
