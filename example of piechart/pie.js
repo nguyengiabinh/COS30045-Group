33282fc641bf9ccc2cda93fe29c015df8c479467
nguyengiabinh <115540912+nguyengiabinh@users.noreply.github.com>
2024-03-25 01:31:02 +0700

    update pie chart + update data cleaning

--- example of piechart/pie.js
+++ example of piechart/pie.js
@@ -42,7 +42,7 @@ d3.csv("event_data_pse.csv").then(function(data) {
         .attr("class", "img");
 
     // Initialize the radius for the pie chart
-    var outerRadius = w / 5;
+    var outerRadius = w / 4;
     var innerRadius = 50;
 
     // Tell D3 how to draw each slice of the circle
@@ -59,7 +59,7 @@ d3.csv("event_data_pse.csv").then(function(data) {
         .data(pie(dataset))
         .enter()
         .append("g")
-        .attr("class", "arc")
+        .attr("class", "arc hoverable")
         .attr("transform", "translate(" + centerX + "," + centerY + ")");
 
         arcs.append("path")
@@ -97,38 +97,50 @@ d3.csv("event_data_pse.csv").then(function(data) {
             return Object.keys(counts)[i] + ": " + dataset[i];
         });
 
-    // Add legend
-    var legend = svg.selectAll(".legend")
-        .data(Object.keys(counts))
-        .enter().append("g")
-        .attr("class", "legend")
-        .attr("transform", function(d, i) {
-            return "translate(0," + i * 20 + ")";
-        });
+// Add legend
+var legend = svg.selectAll(".legend")
+    .data(Object.keys(counts).map(function(key, index) {
+        return {
+            name: key,
+            value: dataset[index], // Add the corresponding value from the dataset
+            color: colors[index % colors.length] // Assign color based on index
+        };
+    }))
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", function(d, i) {
+        return "translate(0," + i * 20 + ")";
+    });
 
-    legend.append("rect")
-        .attr("class", "hover-rect")
-        .attr("x", w - 18)
-        .attr("width", 18)
-        .attr("height", 18)
-        .style("fill", function(d, i) {
-            return colors[i % colors.length]; // Assign color based on index
-        });
+legend.append("rect")
+    .attr("class", "hoverable")
+    .attr("x", w - 18)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", function(d) {
+        return d.color;
+    });
 
-        legend.on("mouseover", function (event, d, i) {
-            // Show text label on hover
-            d3.select(this).append("text")
-                .attr("class", "hover-label")
-                .attr("x", w - 24)
-                .attr("y", 9)
-                .attr("dy", ".35em")
-                .style("text-anchor", "end")
-                .text(function(d) {
-                    return d.replace(/\ufffd/g, ''); // Replace all occurrences of U+00a0 with an empty string
-                });
-        })
-        .on("mouseout", function () {
-            // Remove the text label on mouseout
-            d3.select(this).select(".hover-label").remove();
-        });
+legend.on("mouseover", function(event, d, i) {
+
+    d3.select(this).transition()
+        .duration(100)
+        .attr("opacity", 0.7);
+
+    // Show text label on hover
+    svg.append("text")
+        .attr("class", "legend-hover-label")
+        .attr("x", centerX) // Position it at the center of the pie chart
+        .attr("y", h/2 + w/3.5) // Position it below the pie chart
+        .attr("text-anchor", "middle")
+        .text(d.name.replace(/\ufffd/g, '') + ": " +d.value + "~" + ((d.value / d3.sum(dataset)) * 100).toFixed(2) + "%" ); // Display the legend item's name
+})
+.on("mouseout", function () {
+    // Remove the text label on mouseout
+    svg.select(".legend-hover-label").remove();
+
+    d3.select(this).transition()
+        .duration(100)
+        .attr("opacity", 1);
+});
 });
